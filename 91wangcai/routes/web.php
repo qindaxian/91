7353c73e5c9374b91a73d6eb124c41563e7665c5
DESKTOP-DMJJU75\Feng Yikai <feng_yikai1999@163.com>
2018-04-17 20:24:04 +0800

    冯一锴-注册登陆

---
+++ 91wangcai/routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['namespace' => 'Admin'], function(){
+    // 控制器在 "App\Http\Controllers\Admin" 命名空间下
+   	Route::get('admin/index','IndexController@index');
+   	Route::get('admin/product-project','ProductController@project');
+   	Route::get('admin/product-creditor','ProductController@creditor');
+});
+
+//中间件
+Route::group(['middleware' => ['web','admin.login.login']], function () {
+    Route::get('/admin/login/login','Admin\LoginController@login');
+    //登录页面
+    Route::get('/admin/index/index','Admin\IndexController@index');
+    //后台首页路由
+});
+
+
+Route::group(['namespace' => 'Home'], function(){
+    // 控制器在 "App\Http\Controllers\Admin" 命名空间下
+   	//前台首页
+	Route::get('home/index', 'IndexController@index');
+	//前台帮助中心
+	Route::get('home/helpcenter', 'HelpcenterController@index');
+	//前台信息披露
+	Route::get('home/informationdisclosure', 'InformationdisclosureController@index');
+	//前台出借金额
+	Route::get('home/loan', 'LoanController@index');
+	//前台运营报告
+	Route::get('home/operationreport', 'OperationreportController@index');
+	//前台项目列表
+	Route::get('home/projectlist', 'ProjectlistController@index');
+	//前台旺财记事
+	Route::get('home/chronicle', 'ChronicleController@index');
+	//用户注册
+    Route::get('home/reg', 'IndexController@reg');
+    //短信验证码
+    Route::get('home/registers','IndexController@loginDo');
+    //注册账号
+    Route::post('/doreg','IndexController@doReg');
+    //用户登陆
+    Route::post('/login','IndexController@login');
+    //登陆成功
+    Route::get('home/user','IndexController@user');
+});
