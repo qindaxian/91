7b09473ff89b2d75bdc6900215f48f8b0e63b36b
QINLEVEL\QinLevel <411559489@qq.com>
2018-04-24 08:47:24 +0800

    admin-list

--- 91wangcai/app/Http/Controllers/Admin/RoleController.php
+++ 91wangcai/app/Http/Controllers/Admin/RoleController.php
@@ -33,8 +33,9 @@ class RoleController extends Controller
     	foreach ($a_id as $ke => $val) {
     	 	
     		$user = DB::select('select * from admin_a_r where a_id = ?', [$val]);
+
     		foreach($user as $k=>$v){  
-		    $user[$k]=(array)$v;             
+		    	$user[$k]=(array)$v;             
 			}  
 
 			foreach ($user as $key => $value) {
@@ -46,76 +47,54 @@ class RoleController extends Controller
 			
 		 }
 
+		 $new_arr = [];
 
-
-
-		 foreach ($arr as $oneKey => $oneValue) {
-		 	$r_id[$oneValue[0]['a_name']] = $oneValue[0]['r_id'];
-		 	// $res = DB::select('select * from admin_role where a_id = ?', [$oneValue[0][r_id]]);
-		 	foreach ($r_id as $twoKey => $twoValue) {
-		 	    $res = DB::select('select * from admin_role where r_id = ?', [$twoValue]);
-		 	    foreach($res as $k=>$v){  
-			      $res[$k]=(array)$v;             
-				} 
-		 	}
-
-
+		 foreach ($arr as $a_k => $a_v) {
+		 	$new_arr[] = $a_v[0];
 		 }
+
+    	$r_id = array_column($new_arr,'r_id');
+
+    	 
+		 foreach ($r_id as $k_e => $v_a) {
+		 	$res = DB::select('select * from admin_role where r_id = ?', [$v_a]);
+		 	foreach($res as $k=>$v){  
+			     $res[$k]=(array)$v;             
+			}
+			foreach ($res as $k_y => $v_e) {
+				$low_arr[] = [
+					$k_y => $v_e,
+				];
+			}
+		 }
+		 foreach ($low_arr as $a_k => $a_v) {
+		 		$two_arr[] = $a_v[0];
+		 }
+
+
+		$new_tow = [];
+		foreach ($two_arr as $k => $v){
+			$new_tow[$v['r_id']][]=$v;
+		}
+		foreach ($new_arr as $k => $v) {
+			$new_tow[$v['r_id']][]['a_name']=$v['a_name'];
+		}
+		$new_new_tow=[];
+		foreach ($new_tow as $k => $v) {
+			$a=array_column($v,'a_name');
+			$str=implode(',',$a);
+			$v[0]['a_name']=$str;
+			$new_new_tow[$k][]=$v[0];
+		}
+
+		$end_arr = [];
+		foreach ($new_new_tow as $a_k => $a_v) {
+		 		$end_arr[] = $a_v[0];
+		 }
+		$num = count($end_arr,0);
 		
-    	
-    	print_r($res);die;
-
-    	$role_obj = new Admin_roleModel;
-    	$r_data = $role_obj->getAll();
-
-    	$a_r_obj = new Admin_a_rModel;
-    	$r_id = $a_r_obj->getAll();
-
-		// 管理员姓名/管理员id/角色id/角色名字/角色描述
-
-
-		// $arr = [
-		// 	'角色id' => [
-		// 		'角色名字' => 'aaa',
-		// 		'角色描述' => '啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊b',
-		// 		'管理员名字' => [
-		// 			'a','b','c'
-		// 		]
-		// 	]
-		// ];
-
-		// $arr['角色id']['管理员名字'][] = $val['管理员名字'];
-
-
-    	
-    	// print_r($r_id);die;
-    	// $arr = [];
-
-    	// foreach ($a_id as $key => $value) {
-    	// 	foreach ($r_id as $ke => $val) {
-    	// 		if($key[a_id] = $ke[a_id]){
-    				
-    	// 		}
-    	// 	}
-    	// }
-    	// die;
-    		
-
-        	
-    	
-
-
-    	// $results = DB::select('select * from users where id = :id', ['id' => 1]);
-
-	    // $r_id = RoleController::select(DB::raw('*'))
-	    // ->where('a_id', $a_id)
-	    // ->groupBy('a_id')
-	    // ->skip($offset)
-	    // ->take($limit)
-	    // ->get();
-    
-
-        return view('admin/role/role');
+		
+        return view('admin/role/role',['end_arr' => $end_arr,'num' => $num]);
     }
 
 
